d4719efde3c00555a75348534403bee18edd1a72
Aníbal Álvarez <anibal.alvarezg@gmail.com>
2021-05-23 20:10:00 -0400

    Unused files are removed

--- src/app/components/edit-button/edit-button.component.ts
+++ src/app/components/edit-button/edit-button.component.ts
@@ -3,7 +3,6 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-edit-button',
   templateUrl: './edit-button.component.html',
-  styleUrls: ['./edit-button.component.scss']
 })
 export class EditButtonComponent {
   @Output() clickHandler = new EventEmitter<boolean>();
